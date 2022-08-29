67da061231d8d318192535f2a80341361f1cab77
Elena Patcuk <1999prsmx@.gmail.com>
2022-08-29 17:29:52 +0700

    slider

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
-window.addEventListener('DOMContentLoaded', ()=>{
-    const body = document.querySelector('body');
-    let textNodes = []
+const slides = document.querySelectorAll('.slide')
 
-    function recursy(element){
-        element.childNodes.forEach(node=>{
+for(const slide of slides){
+    slide.addEventListener('click', () =>{
+        clearActiveClesses()
 
-            if (node.nodeName.match(/^H\d/)){
-                const obj = {
-                    header: node.nodeName, 
-                    content: node.textContent
-                }
-                textNodes.push(node.textContent)
-            }else{
-                recursy(node)
-            }
-        })
-    }
-
-    recursy(body)    
-    
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST', 
-        headers:{
-            'Content-Type': 'application/json'
-        }, 
-        body: JSON.stringify(textNodes)
+        slide.classList.add('active')
     })
-    .then(response => response.json())
-    .then(json => console.log(json))
-})
+}
+
+function clearActiveClesses() {
+    slides.forEach(slide => {
+        slide.classList.remove('active')
+    })
+}
